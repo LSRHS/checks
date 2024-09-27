638caed7183f285fd1043debdc804ffe286d0c55
Mark Sobkowicz <mark@evtika.net>
2024-09-27 15:06:30 -0400

    Create checkme.py

---
+++ checkme.py
@@ -0,0 +1,8 @@
+import check50
+
+def exists():
+  check50.exists("deep_thought.py")
+  check50.exists("file_extensions.py")
+  check50.exists("math_interpreter.py")
+  check50.exists("meal_time.py")
+
